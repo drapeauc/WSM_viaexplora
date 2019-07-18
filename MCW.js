48de79490c754c7ce0fb65e9b41695de57832b04
unknown <drapeaucharles@gmail.com>
2019-07-18 18:33:52 -0400

    v2

--- MCW.js
+++ MCW.js
@@ -2,11 +2,35 @@
 var log= console.log.bind(console)
 var isArray= Array.isArray
 
+/*mStart
+title = titre HTML
+body = contenu page HTML
+*/
+mStart= function(ops){
+	var {title, body} = ops
+	return `
+	<html>
+		<head>
+			  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+			  <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.css" rel="stylesheet">
+			  <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.js"></script>
+
+				<meta charset="UTF-8">
+				<title>${title}</title>
+		</head>
+<body>
+${body}
+</body>
+<script type="text/javascript">
+  window.mdc.autoInit();
+</script>
+</html>
+`
+}
 /* MCW Icon
  * ariaNotHidden: tf f ne pas metrre le aria hidden
  * children: nom de l'icon
  */
-
 mIcon= function(ops){
     var { icon='', ariaNotHidden,className=''}=ops
     // join ' ' pcq plusieurs icons possible
@@ -31,7 +55,7 @@ mButton= function(ops){
 
 
     return [
-        `<button class="mdc-button ${raised?'mdc-button--raised ':''}${outlined?'mdc-button--outlined ':''}" ${disabled?'disabled':'' b} >`,
+        `<button class="mdc-button ${raised?'mdc-button--raised ':''}${outlined?'mdc-button--outlined ':''}" ${disabled?'disabled':''} >`,
 		icon?mIcon({className:'mdc-button__icon', icon}):'',
         
 		trailingIcon?mIcon({className:'mdc-button__icon', icon:trailingIcon}):'',
@@ -40,18 +64,29 @@ mButton= function(ops){
 }
 
 /*MCW Checkbox
+label: Texte afficher
+checked: Cocher de base ou none
+inderterminate: Inteterminate de base (a travailler)
+disabled: disabled
+icon: avant
+trailingIcon: apres
+value: Valeur de retour (0 ou 1)
+name: Nom
+Id: Id généré
 */
 var idchk=0
-mCheckbox{
-	console.log("mCheckbox got",{ops})
+mCheckbox=function(ops){
 	
-	var {label='', checked, indeterminate, disabled, icon, trailingIcon, value, name, id=`idchk${idchk++}`} = ops
+	var {label='', checked, inderterminate, disabled, icon, trailingIcon, value, name, id=`idchk${idchk++}`} = ops
 	return[`
 	<div class="mdc-form-field">
-		<div class="mdc-checkbox" ${disabled?'mdc-checkbox--disabled':''} ${value?`'value=${value}':''` ${name?`'name=${name}':''`} ${inderterminate?'aria-checked="mixed"':''} ${checked?'checked':''}>
-			<input type="checkbox"
+		<div class="mdc-checkbox">
+			<input type="checkbox" ${value?`value=${value}`:''}${name?`name=${name}`:''}
 				   class="mdc-checkbox__native-control"
-				   id=${id} ${disabled?'disabled':disabled}/>
+				   id=${id} ${disabled?'disabled':''}
+				   ${disabled?'mdc-checkbox--disabled':''} 
+				   ${inderterminate?'aria-checked="mixed"':''} 
+				   ${checked?'checked':''}/>
 			<div class="mdc-checkbox__background">
 			  <svg class="mdc-checkbox__checkmark"
 				   viewBox="0 0 24 24">
@@ -62,15 +97,67 @@ mCheckbox{
 			  <div class="mdc-checkbox__mixedmark"></div>
 			</div>
 		  </div>
-		${label?`'<label for=id>${label}</label>'`:''}
+		${label?`<label for=${id}>${label}</label>`:''}
 	</div>	
 	`]
 	
 }
 
+/*mSwitches
 
-
-/* AFAIRE CHARLES */
-mPageMCW= function(ops,...children){ return children.join('\n')}
-
-module.exports = { mButton, mIcon, mPageMCW }
+*/
+var idswi=0
+mSwitches=function(ops){
+	
+	var {label='',checked,disabled,icon,trailingIcon,value,name,id=`idswi${idswi++}`} = ops
+	return[` 
+<div class="mdc-switch ${disabled?'mdc-switch--disabled':''} ${checked?'mdc-switch--checked':''}" data-mdc-auto-init="MDCSwitch" >
+	<div class="mdc-switch__track"></div>
+	<div class="mdc-switch__thumb-underlay">
+		<div class="mdc-switch__thumb">
+        <input type="checkbox" id=${id} class="mdc-switch__native-control" role="switch" ${value?`value="${value}"`:''} ${name?`name="${name}"`:''} ${disabled?'disabled':''} ${checked?'checked':''}>
+		</div>
+	</div>
+</div>
+<label for=${id}>${label}</label>
+	`]
+}
+var idtxt=0
+mTexte=function(ops){ 
+	var {label='',maxL="140",helper,disabled,icon,trailingIcon,value,name,id=`idtxt${idtxt++}`} = ops
+	return[` 
+	<div class="mdc-text-field ${disabled?'mdc-text-field--disabled':''}" data-mdc-auto-init="MDCTextField">
+		<input type="text" id=${id} class="mdc-text-field__input" ${disabled?'disabled':''} ${value?`value="${value}"`:''}>
+		<label class="mdc-floating-label ${value?'mdc-floating-label--float-above':''}for=${id}">${label}</label>
+		<div class="mdc-line-ripple"></div>
+	</div>
+	${helper?`<div class="mdc-text-field-helper-line">
+	<div class="mdc-text-field-helper-text">${helper}</div>
+	</div> ` :''}
+	
+	`]
+}
+/*mMultitexte
+Problème avec: 
+*/
+var idmultitxt=0
+mMultitexte=function(ops){
+	var {label='',rows="8",maxL="140",cols="40",helper,disabled,icon,trailingIcon,value,name,id=`idmultitxt${idmultitxt++}`} = ops
+ return[` 
+  <div class="mdc-text-field mdc-text-field--textarea ${disabled?'mdc-text-field--disabled':''}" data-mdc-auto-init="MDCTextField">
+  <div class="mdc-text-field-character-counter">0 / ${maxL}</div>
+  <textarea id=${id} class="mdc-text-field__input" rows="${rows}" cols="${cols}" maxlength="${maxL}" ${disabled?'disabled':''}>${value?`${value}`:''}</textarea>
+  <div class="mdc-notched-outline">
+    <div class="mdc-notched-outline__leading"></div>
+    <div class="mdc-notched-outline__notch">
+      <label for=${id} class="mdc-floating-label" >${label}</label>
+    </div>
+    <div class="mdc-notched-outline__trailing"></div>
+  </div>
+</div>
+${helper?`<div class="mdc-text-field-helper-line">
+	<div class="mdc-text-field-helper-text">${helper}</div>
+	</div> ` :''}
+`]
+}
+module.exports = { mButton, mIcon, mCheckbox, mSwitches,mStart, mTexte, mMultitexte}
--- TestMCW.js
+++ TestMCW.js
@@ -1,15 +1,31 @@
 
 var MCW= require('./MCW.js')
 
-var { mPageMCW, mButton } = MCW
+body=[
+	mSwitches({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Normal",value:"salut les amis"}),
+	mSwitches({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Checked",value:"salut les amis",checked:"true"}),
+	mSwitches({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Disabled",value:"salut les amis",disabled:"true"}),
+	"<br>",
+	mCheckbox({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Normal",value:"salut les amis"}),
+	mCheckbox({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Checked",value:"salut les amis",checked:"true"}),
+	mCheckbox({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Disabled",value:"salut les amis",disabled:"true"}),
+	"<br>",
+	mTexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Normal"}),
+	mTexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"disabled",disabled:"TRUE"}),
+	"<br>",
+	mTexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"value",value:"je suis la value"}),
+	mTexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"helper",helper:"je suis le helper"}),
+	"<br>",
+	mMultitexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"multi - ligne",row:"10"}),
+	mMultitexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"disable",disabled:"10"}),
+	"<br>",
+	"<br>",
+	mMultitexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"value",value:"je suis la value"}),
+	mMultitexte({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Helper",helper:"je suis le helper"}),
+	''].join('')
+ 
 
-test=mPageMCW({},
-  mButton({label:'bouton simple'}),
-  '<br/>',
-  mButton({primary:0,outline:1,icon:'stop',trailingIcon:'start',label:"Stop/Start"},
-    ),
- ''
- )
 
 
-console.log(test)
+console.log(mStart({title:"Charles", body:body},
+))
